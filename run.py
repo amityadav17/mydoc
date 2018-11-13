89e5a808ad03336ea717ad3e58a52bfd654eeeec
amityadav17 <amit.yadav.1703@gmail.com>
2018-11-13 21:10:18 +0530

    Procfile update

--- run.py
+++ run.py
@@ -1,16 +1,19 @@
 import os
 import sys
-import SimpleHTTPServer
-import SocketServer
+#import SimpleHTTPServer
+#import SocketServer
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-PORT = int(os.environ.get("PORT", 8000))
+PORT = int(os.environ.get("PORT", 5000)
 
 
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+#Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 
-httpd = SocketServer.TCPServer(("", PORT), Handler)
+#httpd = SocketServer.TCPServer(("", PORT), Handler)
 
+command = "python -m http.server " + PORT,
+print(command)
+os.system(command)
 print("serving at port", PORT)
 httpd.serve_forever()
